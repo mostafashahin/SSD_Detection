b8947b069c21c3fba7175586686bb4ebbad6b1b1
mostafashahin <mostafa_shahin@ieee.org>
2019-02-19 12:31:31 +0200

    Adding DataPreparation Folder

---
+++ DataPreparation/Prepare_Ultra_Data.py
@@ -0,0 +1,44 @@
+import glob,os
+from os.path import normpath, splitext, join, isfile
+def Get_Segnments_From_TextGrid_Short(TextGrid_File,Speaker='CHILD'):
+    with open(TextGrid_File,'r') as fTextGrid:
+        TextGrid_content = fTextGrid.read().splitlines()
+    aSegments = [(TextGrid_content[i-2],TextGrid_content[i-1]) for i in range(len(TextGrid_content)) if TextGrid_content[i].find(Speaker)!=-1]
+    return aSegments
+
+def Select_Data(Dataset_Folder, TextGrid_Folder, Sessions='', Tasks=[]):
+    #List All wavs in the Wave_Folder recercievely
+    dWaves_Segments = {}
+    lWave_Files = glob.glob(join(Dataset_Folder,'**','*[' + ''.join(Tasks) + '].wav'), recursive=True)
+    #print(lWave_Files)
+    for sWave_File in lWave_Files:
+        if Sessions=='':
+            sSpeakerID, sUttID = normpath(splitext(sWave_File)[0]).split(os.sep)[-2:]
+            sTextGrid_Name = '-'.join([sSpeakerID, sUttID])+'.TextGrid'
+            sTextGrid_File = join(TextGrid_Folder,sTextGrid_Name)
+            if isfile(sTextGrid_File):
+                dWaves_Segments[sWave_File] = Get_Segnments_From_TextGrid_Short(sTextGrid_File)
+            else:
+                print(sTextGrid_File)
+            #lWavs_TextGrids.append((sWave_File,os.path.join(TextGrid_Folder,sTextGrid_Name)))
+        else:
+            sSpeakerID, sSessionID, sUttID = os.path.normpath(splitext(sWave_File)[0]).split(os.sep)[-3:]
+            if sSessionID in Sessions:
+                sTextGrid_Name = '-'.join([sSpeakerID, sSessionID, sUttID])+'.TextGrid'
+                sTextGrid_File = join(TextGrid_Folder,sTextGrid_Name)
+                if isfile(sTextGrid_File):
+                    dWaves_Segments[sWave_File] = Get_Segnments_From_TextGrid_Short(sTextGrid_File)
+                else:
+                    print(sTextGrid_File)
+                #lWavs_TextGrids.append((sWave_File,os.path.join(TextGrid_Folder,sTextGrid_Name)))
+    return dWaves_Segments
+
+def Write_Wave_Segments_To_File(dWaves_Segments,sOutput_File):
+    with open(sOutput_File,'w') as fOutput_File:
+        for sWave_File in dWaves_Segments:
+            for tSegment in dWaves_Segments[sWave_File]:
+                print(';'.join([sWave_File] + [str(i) for i in tSegment]), file = fOutput_File)
+    return
+
+
+#def Generate
