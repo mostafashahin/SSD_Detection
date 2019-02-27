3d934e1fd0b902e37c53bf80fafc2abff44f8b3f
mostafashahin <mostafa_shahin@ieee.org>
2019-02-27 13:31:45 +0200

    Make the Select Data function take list of selected speakers as arguments and filter data accordingly

--- DataPreparation/Prepare_Ultra_Data.py
+++ DataPreparation/Prepare_Ultra_Data.py
@@ -6,14 +6,19 @@ def Get_Segnments_From_TextGrid_Short(TextGrid_File,Speaker='CHILD'):
     aSegments = [(TextGrid_content[i-2],TextGrid_content[i-1]) for i in range(len(TextGrid_content)) if TextGrid_content[i].find(Speaker)!=-1]
     return aSegments
 
-def Select_Data(Dataset_Folder, TextGrid_Folder, Sessions='', Tasks=[]):
+def Select_Data(Dataset_Folder, TextGrid_Folder, Sessions='', Tasks=[], Train_Spkrs_List=[],Test_Spkrs_List=[]):
     #List All wavs in the Wave_Folder recercievely
-    dWaves_Segments = {}
+    dWaves_Segments_Train = {}
+    dWaves_Segments_Test  = {}
     lWave_Files = glob.glob(join(Dataset_Folder,'**','*[' + ''.join(Tasks) + '].wav'), recursive=True)
     #print(lWave_Files)
     for sWave_File in lWave_Files:
         if Sessions=='':
             sSpeakerID, sUttID = normpath(splitext(sWave_File)[0]).split(os.sep)[-2:]
+            if sSpeakerID in Train_Spkrs_List:
+                dWaves_Segments = dWaves_Segments_Train
+            else:
+                dWaves_Segments = dWaves_Segments_Test
             sTextGrid_Name = '-'.join([sSpeakerID, sUttID])+'.TextGrid'
             sTextGrid_File = join(TextGrid_Folder,sTextGrid_Name)
             if isfile(sTextGrid_File):
@@ -23,6 +28,10 @@ def Select_Data(Dataset_Folder, TextGrid_Folder, Sessions='', Tasks=[]):
             #lWavs_TextGrids.append((sWave_File,os.path.join(TextGrid_Folder,sTextGrid_Name)))
         else:
             sSpeakerID, sSessionID, sUttID = os.path.normpath(splitext(sWave_File)[0]).split(os.sep)[-3:]
+            if sSpeakerID in Train_Spkrs_List:
+                dWaves_Segments = dWaves_Segments_Train
+            else:
+                dWaves_Segments = dWaves_Segments_Test
             if sSessionID in Sessions:
                 sTextGrid_Name = '-'.join([sSpeakerID, sSessionID, sUttID])+'.TextGrid'
                 sTextGrid_File = join(TextGrid_Folder,sTextGrid_Name)
@@ -31,7 +40,7 @@ def Select_Data(Dataset_Folder, TextGrid_Folder, Sessions='', Tasks=[]):
                 else:
                     print(sTextGrid_File)
                 #lWavs_TextGrids.append((sWave_File,os.path.join(TextGrid_Folder,sTextGrid_Name)))
-    return dWaves_Segments
+    return dWaves_Segments_Train, dWaves_Segments_Test
 
 def Write_Wave_Segments_To_File(dWaves_Segments,sOutput_File):
     with open(sOutput_File,'w') as fOutput_File:
@@ -45,9 +54,13 @@ def Extract_Features_openSmile(sWave_Segments_File, sConfig_File='../openSmile/c
     with open(sWave_Segments_File) as fSegments:
         for sLine in fSegments:
             sWave_File, sStart, sEnd = sLine.split(';')
-            command = ['../openSmile/bin/SMILExtract','-C',sConfig_File,'-I',sWave_File,'-start',sStart,'-end',sEnd,'-instname',sWave_File,'-csvoutput',sSegment_Level_csv_File]
+            print('Now Processing, ', sWave_File, ' From ', sStart, ' To ', sEnd )
+            command = ['SMILExtract','-C',sConfig_File,'-I',sWave_File,'-start',sStart,'-end',sEnd,'-instname',sWave_File,'-class','1','-csvoutput',sSegment_Level_csv_File]
             subprocess.run(command)
     return
 
+#def Split_Wavs_Train_Test_From_Speaker_List():
+#    #i/p --> Array of tuples each (list_of_waves,list_train_speakers,list_test_speakers,class)
+    
 
 
